e6d484e8a302553c38227d0fdfd9060d524761c1
Evelyn Gálvez <evelyn.galvez.p@gmail.com>
2018-10-12 12:41:55 -0300

    agregados contenedores para pantalla de proyectos y contacto, animación para transición de cada una también

--- docs/app.js
+++ docs/app.js
@@ -1,4 +1,27 @@
-function aboutMe() {
+const aboutMe = () => {
   document.getElementById('particles-js').style.display = 'none';
   document.getElementById('aboutMe').style.display = 'block';
+  document.getElementById('projects').style.display = 'none';
+  document.getElementById('contact').style.display = 'none';
+}
+
+const projects = () => {
+  document.getElementById('particles-js').style.display = 'none';
+  document.getElementById('projects').style.display = 'block';
+  document.getElementById('contact').style.display = 'none';
+  document.getElementById('aboutMe').style.display = 'none';
+}
+
+const contact = () => {
+  document.getElementById('particles-js').style.display = 'none';
+  document.getElementById('contact').style.display = 'block';
+  document.getElementById('projects').style.display = 'none';
+  document.getElementById('aboutMe').style.display = 'none';
+}
+
+const home = () => {
+  document.getElementById('particles-js').style.display = 'block';
+  document.getElementById('contact').style.display = 'none';
+  document.getElementById('projects').style.display = 'none';
+  document.getElementById('aboutMe').style.display = 'none';
 }

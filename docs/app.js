25372faaf74c2f90d50d5ddf15851bf257366203
Evelyn Gálvez <evelyn.galvez.p@gmail.com>
2018-10-17 17:27:13 -0300

    diseño movil en pantalla aboutMe

--- docs/app.js
+++ docs/app.js
@@ -20,8 +20,11 @@ const contact = () => {
 }
 
 const home = () => {
-  document.getElementById('particles-js').style.display = 'block';
+  document.getElementById('particles-js').className = 'animated fadeInUp';
   document.getElementById('contact').style.display = 'none';
   document.getElementById('projects').style.display = 'none';
   document.getElementById('aboutMe').style.display = 'none';
+  //document.getElementById('particles-js').classList.remove('');
+  
+  document.getElementById('particles-js').style.display = 'block';
 }

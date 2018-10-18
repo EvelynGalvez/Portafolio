ae6def2bb10cf179f0477bd7ac159972366263c1
Evelyn Gálvez <evelyn.galvez.p@gmail.com>
2018-10-18 03:20:57 -0300

    diseño final versión móvil

--- docs/app.js
+++ docs/app.js
@@ -2,18 +2,28 @@ const aboutMe = () => {
   document.getElementById('portada').style.display = 'none';
   document.getElementById('aboutMe').style.display = 'block';
   document.getElementById('projects').style.display = 'none';
+  document.getElementById('contact').style.display = 'none';
 }
 
 const projects = () => {
   document.getElementById('portada').style.display = 'none';
   document.getElementById('projects').style.display = 'block';
   document.getElementById('aboutMe').style.display = 'none';
+  document.getElementById('contact').style.display = 'none';
 }
 
 const home = () => {
   document.getElementById('portada').style.display = 'block';
-  document.getElementById('portada').className = 'animated fadeInUp';
+  document.getElementById('portada').className = 'animated fadeIn slow';
   document.getElementById('projects').style.display = 'none';
   document.getElementById('aboutMe').style.display = 'none';
+  document.getElementById('contact').style.display = 'none';
   //document.getElementById('particles-js').classList.remove('');
+}
+
+const contact = () => {
+  document.getElementById('portada').style.display = 'none';
+  document.getElementById('contact').style.display = 'block';
+  document.getElementById('projects').style.display = 'none';
+  document.getElementById('aboutMe').style.display = 'none';
 }

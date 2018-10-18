c8e80b47c05fda38c95290e52cb4d2610444e6ae
Evelyn Gálvez <evelyn.galvez.p@gmail.com>
2018-10-17 23:22:08 -0300

    solucionado problema diseño canvas

--- docs/app.js
+++ docs/app.js
@@ -1,30 +1,19 @@
 const aboutMe = () => {
-  document.getElementById('particles-js').style.display = 'none';
+  document.getElementById('portada').style.display = 'none';
   document.getElementById('aboutMe').style.display = 'block';
   document.getElementById('projects').style.display = 'none';
-  document.getElementById('contact').style.display = 'none';
 }
 
 const projects = () => {
-  document.getElementById('particles-js').style.display = 'none';
+  document.getElementById('portada').style.display = 'none';
   document.getElementById('projects').style.display = 'block';
-  document.getElementById('contact').style.display = 'none';
-  document.getElementById('aboutMe').style.display = 'none';
-}
-
-const contact = () => {
-  document.getElementById('particles-js').style.display = 'none';
-  document.getElementById('contact').style.display = 'block';
-  document.getElementById('projects').style.display = 'none';
   document.getElementById('aboutMe').style.display = 'none';
 }
 
 const home = () => {
-  document.getElementById('particles-js').className = 'animated fadeInUp';
-  document.getElementById('contact').style.display = 'none';
+  document.getElementById('portada').style.display = 'block';
+  document.getElementById('portada').className = 'animated fadeInUp';
   document.getElementById('projects').style.display = 'none';
   document.getElementById('aboutMe').style.display = 'none';
   //document.getElementById('particles-js').classList.remove('');
-  
-  document.getElementById('particles-js').style.display = 'block';
 }
